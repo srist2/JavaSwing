1e53d1a1b11f7d52359b1a641d2be8fb6e15f2c7
srist2 <52330627+srist2@users.noreply.github.com>
2020-11-23 15:29:34 +0800

    Update 112301.App.java

--- 112301.App.java
+++ 112301.App.java
@@ -10,15 +10,12 @@ public class App {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int array[][] = {{10,50}, {20, 35}, {30, 25}, {40, 70}};
 		while (true) {
 			System.out.println("用户输入");
 			Scanner scanner = new Scanner(System.in);
 			String  input_str = scanner.nextLine();
-			Random random = new Random();
-			int randomX = random.nextInt(4);
 			if (input_str.equals("a")) {
-				System.out.println("坐标" + array[randomX][0] + "Y坐标" + array[randomX][1]);
+				System.out.println("X坐标" + (int)(Math.random()*100) + "Y坐标" + (int)(Math.random()*100));
 			}
 		}
 	}
