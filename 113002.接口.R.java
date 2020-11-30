0b3d36340ad971faea6374a40c4e9f7ae4e943e9
srist2 <52330627+srist2@users.noreply.github.com>
2020-11-30 15:06:38 +0800

    Update 113002.接口.R.java

--- 113002.接口.R.java
+++ 113002.接口.R.java
@@ -1,7 +1,7 @@
 package pkg;
 
 public interface R {
-	static String boy_filePath = "img/boy.gif";
-	static String girl_filePath = "img/girl.gif";
-	static int[][]array2d = {{1, 2, 3}, {4, 5, 6}};
+	String boy_filePath = "img/boy.gif";
+	String girl_filePath = "img/girl.gif";
+	int[][]array2d = {{1, 2, 3}, {4, 5, 6}};
 }
