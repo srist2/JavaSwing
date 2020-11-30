be471baae1156d7679369d04d5463ba0b3a81c5f
srist2 <52330627+srist2@users.noreply.github.com>
2020-11-30 14:58:01 +0800

    Create 113001.继承子类.App.java

---
+++ 113001.继承子类.App.java
@@ -0,0 +1,19 @@
+package pkg;
+
+public class App extends R {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("boy_filePath" + boy_filePath);
+		System.out.println("girl_filePath" + girl_filePath);
+		for(int i = 0; i < array2d.length; i++) {
+			for (int j = 0; j < array2d[i].length; j++) {
+				System.out.println(array2d[i][j]);
+			}
+		}
+	}
+
+}
